ca7da8f50a8f30078bbdb915390a9230d4fa8fad
704356116libin <704356116@qq.com>
2019-03-04 21:23:13 +0800

    '知识更新'

--- polls/views.py
+++ polls/views.py
@@ -65,8 +65,9 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+        pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
