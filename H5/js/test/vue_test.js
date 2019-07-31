a8876c29c3bc21db292dc33b26b25e806210026e
彬 <704356116@qq.com>
2019-07-31 22:09:20 +0800

    Vue组件懵逼

--- H5/js/test/vue_test.js
+++ H5/js/test/vue_test.js
@@ -307,3 +307,27 @@ new Vue({
         }
     }
 })
+Vue.component('my-component',function(){
+    template:'\
+    <div>\
+    <h2>我是子组件的标题</h2>\
+    <slot>\
+      只有在没有要分发的内容时才会显示。\
+    </slot>\
+    </div>\
+    '
+})
+Vue.component('app-layout',{
+    template:'\
+    <div class="container">\
+    <header>\
+      <slot name="header"></slot>\
+    </header>\
+    <main>\
+      <slot></slot>\
+    </main>\
+    <footer>\
+      <slot name="footer"></slot>\
+    </footer>\
+  </div>'
+})
