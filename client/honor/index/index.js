d0c8cf68fd97a2a6cf30d6ce2ca241a12f152f03
彬 <704356116@qq.com>
2019-08-23 21:13:09 +0800

    vue重构完成

--- client/honor/index/index.js
+++ client/honor/index/index.js
@@ -1,3 +1,36 @@
+// 商品组件
+Vue.component('shop-list', {
+    props: {
+        main: Object,
+        items: Array
+    },
+    template: `
+    <div>
+        <div class="prod-items">
+            <div class="box-hd">
+                <h3 class="title">{{main.name}}</h3>
+            </div>
+        </div>
+        <div class="item">
+            <a :href="main.url">
+                <img :src="main.url" :alt="main.name">
+            </a>
+        </div>
+        <div class="item" v-for="item in items">
+            <div class="title">{{item.name}}</div>
+            <div class="desc">{{item.dsc}}</div>
+            <a href="item.url" @click="">
+                <img :src="item.url" alt="">
+            </a>
+        </div>
+    </div>
+    `,
+    methods:{
+        detail:function(id){
+            alert("点击了"+id+"商品")
+        }
+    }
+})
 new Vue({
     el: "#index-content",
     data() {
@@ -17,36 +50,43 @@ new Vue({
                         url: '../img/a.jpg',
                         items: [
                             {
+                                id:6,
                                 name:'荣耀8X',
                                 dsc:'千元屏霸',
                                 url:'../img/6.jpg'
                             },
                             {
+                                id:7,
                                 name:'荣耀8X Max',
                                 dsc:'千元大屏 随身影院',
                                 url:'../img/b.jpg'
                             },
                             {
+                                id:8,
                                 name:'荣耀Note 10',
                                 dsc:'更大 更快 更Cool',
                                 url:'../img/c.jpg'
                             },
                             {
+                                id:9,
                                 name:'荣耀Play',
                                 dsc:'突破   让快更快',
                                 url:'../img/6.jpg'
                             },
                             {
+                                id:10,
                                 name:'荣耀9i',
                                 dsc:'突破   让快更快',
                                 url:'../img/d.jpg'
                             },
                             {
+                                id:11,
                                 name:'荣耀畅玩7',
                                 dsc:'小巧全面屏 通话大音量',
                                 url:'../img/e.jpg'
                             },
                             {
+                                id:12,
                                 name:'荣耀10',
                                 dsc:'会 变 色 更 潮 美',
                                 url:'../img/13.jpg'
@@ -61,16 +101,19 @@ new Vue({
                         url: '../img/b1.jpg',
                         items: [
                             {
+                                id:13,
                                 name:'荣耀magicbook-锐龙版',
                                 dsc:'高性能金属轻薄本',
                                 url:'../img/b2.jpg'
                             },
                             {
+                                id:14,
                                 name:'荣耀MagicBook',
                                 dsc:'独显轻薄',
                                 url:'../img/b2.jpg'
                             },
                             {
+                                id:15,
                                 name:'荣耀畅玩平板2',
                                 dsc:'持久畅快  更痛快',
                                 url:'../img/b3.jpg'
@@ -85,16 +128,19 @@ new Vue({
                         url: '../img/c1.jpg',
                         items: [
                             {
+                                id:16,
                                 name:'荣耀手环4-锐龙版',
                                 dsc:'AMOLED触控大彩屏心率手环',
                                 url:'../img/c2.jpg'
                             },
                             {
+                                id:17,
                                 name:'荣耀手表S1',
                                 dsc:'看到改变',
                                 url:'../img/c3.jpg'
                             },
                             {
+                                id:18,
                                 name:'荣耀畅玩手表A1',
                                 dsc:'精致随行  更痛快',
                                 url:'../img/b4.jpg'
@@ -109,16 +155,19 @@ new Vue({
                         url: '../img/d1.jpg',
                         items: [
                             {
+                                id:19,
                                 name:'荣耀路由2',
                                 dsc:'智能体验  快人一步',
                                 url:'../img/d2.jpg'
                             },
                             {
+                                id:20,
                                 name:'荣耀路由X1增强版',
                                 dsc:'不见天线',
                                 url:'../img/d3.jpg'
                             },
                             {
+                                id:21,
                                 name:'荣耀畅玩手表A1',
                                 dsc:'精致随行  更痛快',
                                 url:'../img/d4.jpg'
@@ -133,16 +182,19 @@ new Vue({
                         url: '../img/f1.jpg',
                         items: [
                             {
+                                id:22,
                                 name:'荣耀自拍杆',
                                 dsc:'发现更美的自己',
                                 url:'../img/f2.jpg'
                             },
                             {
+                                id:23,
                                 name:'荣耀小口哨',
                                 dsc:'随时听你的',
                                 url:'../img/f3.jpg'
                             },
                             {
+                                id:24,
                                 name:'荣耀引擎耳机2',
                                 dsc:'一副耳机，两种音乐态度  更痛快',
                                 url:'../img/f4.jpg'
@@ -198,32 +250,4 @@ new Vue({
         }
     },
 
-})
-// 商品组件
-Vue.component('shop-list', {
-    props: {
-        main: Object,
-        items: Object
-    },
-    template: `
-    <div>
-        <div class="prod-items">
-            <div class="box-hd">
-                <h3 class="title">{{main.name}}</h3>
-            </div>
-        </div>
-        <div class="item">
-            <a :href="main.url">
-                <img :src="main.img_src" :alt="main.name">
-            </a>
-        </div>
-        <div class="item" v-for="item in items">
-            <div class="title">{{item.name}}</div>
-            <div class="desc">{{item.dsc}}</div>
-            <a href="item.url" @click="">
-                <img :src="item.url" alt="">
-            </a>
-        </div>
-    </div>
-    `
 })
