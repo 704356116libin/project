bef73986a47f4fe2ce2f25e67a3a9bf9b8013439
bin <704356116@qq.com>
2019-09-09 13:34:46 +0800

    1.引入element-ui准备搭建主页

--- vue_hello/babel.config.js
+++ vue_hello/babel.config.js
@@ -2,22 +2,22 @@ module.exports = {
   presets: [
     '@vue/app'
   ],
-  runtimeCompiler: true,
-  publicPath: '/', // 设置打包文件相对路径
-  devServer: {
-    // open: process.platform === 'darwin',
-    // host: 'localhost',
-    port: 8071,
-    // open: true, //配置自动启动浏览器
-    proxy: {
-      '/api': {
-        target: '', //对应自己的接口
-        changeOrigin: true,
-        ws: true,
-        pathRewrite: {
-          '^/api': ''
-        }
-      }
-    }
-   },
+  // runtimeCompiler: true,
+  // publicPath: '/', // 设置打包文件相对路径
+  // devServer: {
+  //   // open: process.platform === 'darwin',
+  //   // host: 'localhost',
+  //   port: 8071,
+  //   // open: true, //配置自动启动浏览器
+  //   proxy: {
+  //     '/api': {
+  //       target: '', //对应自己的接口
+  //       changeOrigin: true,
+  //       ws: true,
+  //       pathRewrite: {
+  //         '^/api': ''
+  //       }
+  //     }
+  //   }
+  //  },
 }
--- vue_hello/src/main.js
+++ vue_hello/src/main.js
@@ -3,12 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
 axios.defaults.timeout = 5000 // 请求超时
 // axios.defaults.baseURL = 'http://dlf.test'
 axios.defaults.baseURL = 'http://127.0.0.1:8000'
 new Vue({
+  el: '#app',
   router,
   store,
   render: h => h(App)
