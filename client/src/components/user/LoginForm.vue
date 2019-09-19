41df92e54ca95e3c5891fd73957820b9a0871a71
704356116libin <704356116@qq.com>
2019-09-19 08:47:46 +0800

    1

---
+++ client/src/components/user/LoginForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="login-form">
+     <div>账户</div>
+     <div>密码</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm',
+  props: {
+    
+  },
+  data() {
+      return {
+       
+      };
+  },
+  methods: {
+   
+  }
+
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  
+</style>
--- client/src/views/user/login.vue
+++ client/src/views/user/login.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="login">
-    
+    <LoginForm/>
     
   </div>
 </template>
 
 <script>
-/*引入轮播图组件*/
-
+/*引入登陆框组键组件*/
+import LoginForm from '@/components/user/LoginForm.vue'
 export default {
   name: 'login',
   components: {
-   
+   LoginForm
   }
 }
 </script>
