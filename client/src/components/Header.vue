a0786fc6f4dd474a7e6dac86b2b6b2b479892b73
bin <704356116@qq.com>
2020-01-19 15:23:57 +0800

    项目前端编译目录dist

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -8,7 +8,7 @@
     background-color="#fff"
     text-color="#000"
     active-text-color="#909399">
-    <el-menu-item index="1">处理中心</el-menu-item>
+    <el-menu-item index="1">处理中心11</el-menu-item>
     <el-submenu index="2">
       <template slot="title">我的工作台</template>
       <el-menu-item index="2-1">选项1</el-menu-item>
